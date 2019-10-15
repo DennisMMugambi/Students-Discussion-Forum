8ca124486372475d04b99667a6ae1cc5802d0aaf
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2019-10-15 10:20:19 +0300

    add section to input post or question

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import './index.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
   return (
-    <div>
-      HelloWorld
+    <div className = "card modal-dialog modal-xl">
+        <div class = "card-body">
+        <textarea className= "form-control card-input"/>
+        <button className= "btn btn-danger card-button">
+            Post
+        </button>
+        </div>
     </div>
   );
 }
